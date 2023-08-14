bc6009594618ce4813fdb94e6512ad9b9dbc775b
kai <cgk0910@gmail.com>
2023-08-14 10:37:41 +0800

    建立會員註冊頁面

---
+++ view/front/reg.php
@@ -0,0 +1,30 @@
+<h2 class="ct">會員註冊</h2>
+<form action="#">
+    <table class="all">
+        <tr>
+            <td class="tt ct">姓名</td>
+            <td class="pp"><input type="text" name="name" id="name"></td>
+        </tr>
+        <tr>
+            <td class="tt ct">帳號</td>
+            <td class="pp"><input type="text" name="acc" id="acc"></td>
+        </tr>
+        <tr>
+            <td class="tt ct">密碼</td>
+            <td class="pp"><input type="text" name="pw" id="pw"></td>
+        </tr>
+        <tr>
+            <td class="tt ct">電話</td>
+            <td class="pp"><input type="text" name="tel" id="tel"></td>
+        </tr>
+        <tr>
+            <td class="tt ct">住址</td>
+            <td class="pp"><input type="text" name="addr" id="addr"></td>
+        </tr>
+        <tr>
+            <td class="tt ct">電子信箱</td>
+            <td class="pp"><input type="text" name="email" id="email"></td>
+        </tr>
+    </table>
+    <div class="ct"><input type="button" value="註冊"><input type="reset" value="重置"></div>
+</form>
