84fa1f703ac2ae9d419a018e3b02df611d7880a5
kai <cgk0910@gmail.com>
2023-08-14 09:15:54 +0800

    先完成購物流程及最新消息頁面

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
                 <a href="?do=login">會員登入</a> |
                 <a href="?do=admin">管理登入</a>
             </div>
-            情人節特惠活動 &nbsp; 為了慶祝七夕情人節，將舉辦情人兩人到現場有七七折之特惠活動~
+            <marquee>年終特賣會開跑了 情人節特惠活動</marquee>
         </div>
         <div id="left" class="ct">
             <div style="min-height:400px;">
@@ -37,6 +37,19 @@
             </span>
         </div>
         <div id="right">
+        <?php
+            $do=$_GET['do']??'main';
+            $table=ucfirst($do);
+            $file="./view/front/{$do}.php";
+
+            if(isset($$table)){
+                $$table->front();
+            }else if(file_exists($file)){
+                include $file;
+            }else{
+                include "./view/front/main.php";
+            }
+        ?>  
         </div>
         <div id="bottom" style="line-height:70px;background:url(icon/bot.png); color:#FFF;" class="ct">
             頁尾版權 : </div>
