a89ceecd059ba3bc2d81ca1417cd4f7a0e968cf9
mackliu <macklun@gmail.com>
2023-08-15 08:47:54 +0800

    建立商品分類輸入表單畫面

---
+++ view/backend/th.php
@@ -0,0 +1,15 @@
+<h2 class="ct">商品分類</h2>
+<div class="ct">
+    <label for="">新增大分類</label>
+    <input type="text" name="big" id="big">
+    <button>新增</button>
+</div>
+<div class="ct">
+    <label for="">新增中分類</label>
+    <select name="bigs" id="bigs"></select>
+    <input type="text" name="mid" id="mid">
+    <button>新增</button>
+</div>
+
+
+<h2 class="ct">商品管理</h2>
