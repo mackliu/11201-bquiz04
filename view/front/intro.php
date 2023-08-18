06b3e599c39ad7d7a1af77f870578ff4c49a6aca
mackliu <macklun@gmail.com>
2023-08-18 08:52:34 +0800

    建立商品詳細資料頁面

---
+++ view/front/intro.php
@@ -0,0 +1,54 @@
+<?php 
+$row=$Goods->find($_GET['id']);
+?>
+
+<h2 class="ct"><?=$row['name'];?></h2>
+<style>
+.item *{
+    box-sizing: border-box;
+}
+.item{
+    display: flex;
+}
+.item > div:nth-child(1){
+    width:60%;
+    padding:10px;
+    height:350px;
+    display:flex;
+    justify-content: center;
+    align-items: center;
+}
+.item img{
+    width:100%;
+    height:100%;
+}
+.item > div:nth-child(2){
+    width:40%;
+}
+.info{
+    display:flex;
+    flex-wrap:wrap;
+}
+.info div{
+    margin:1px 0;
+    padding:2px;
+    width:100%;
+}
+</style>
+<div class="item all">
+    <div class="pp">
+        <img src="./upload/<?=$row['img'];?>" alt="">
+    </div>
+    <div class="info">
+        <div class="pp">分類:<?=$Type->nav($row['mid']);?></div>
+        <div class="pp">編號:<?=$row['no'];?></div>
+        <div class="pp">價格:<?=$row['price'];?></div>
+        <div class="pp">詳細說明:<?=$row['intro'];?></div>
+        <div class="pp">庫存量:<?=$row['stock'];?></div>
+    </div>
+</div>
+<div class="ct tt all">
+    購買數量:
+    <input type="number" name="qt" id="qt" value="1">
+    <img src="./icon/0402.jpg" >
+</div>
