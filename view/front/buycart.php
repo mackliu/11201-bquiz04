05e0a8dafdfc41f88b9d2d2b6f5cd3fca6a3f4a9
mackliu <macklun@gmail.com>
2023-08-18 09:36:15 +0800

    利用session來建立購物車機制

--- view/front/buycart.php
+++ view/front/buycart.php
@@ -1,5 +1,19 @@
 <?php
 
+if(isset($_GET['id']) && isset($_GET['qt'])){
+    $_SESSION['cart'][$_GET['id']]=$_GET['qt'];
+}
+
 if(!isset($_SESSION['user'])){
     to("?do=login");
+}
+
+echo "<h1 class='ct'>{$_SESSION['user']}的購物車</h2>";
+
+if(isset($_SESSION['cart'])){
+
+
+    
+}else{
+    echo "<h2 class='ct'>你的購物車中目前沒有商品，請前往賣場購買</h2>";
 }
