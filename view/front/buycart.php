1da15d3fe348111476280dc2d1d3205eecd5ebfc
mackliu <macklun@gmail.com>
2023-08-18 10:30:45 +0800

    列出購物車商品

--- view/front/buycart.php
+++ view/front/buycart.php
@@ -11,9 +11,43 @@ if(!isset($_SESSION['user'])){
 echo "<h1 class='ct'>{$_SESSION['user']}的購物車</h2>";
 
 if(isset($_SESSION['cart'])){
-
-
+?>
+<table class="all">
+    <tr class="tt ct">
+        <td>編號</td>
+        <td>商品名稱</td>
+        <td>數量</td>
+        <td>庫存量</td>
+        <td>單價</td>
+        <td>小計</td>
+        <td>刪除</td>
+    </tr>
+    <?php
     
+    foreach($_SESSION['cart'] as $id => $qt){
+        $row=$Goods->find($id);
+    ?>
+    <tr class="pp ct">
+        <td><?=$row['no'];?></td>
+        <td><?=$row['name'];?></td>
+        <td><?=$qt;?></td>
+        <td><?=$row['stock'];?></td>
+        <td><?=$row['price'];?></td>
+        <td><?=$row['price']*$qt;?></td>
+        <td>
+            <img src="./icon/0415.jpg" alt="">
+        </td>
+    </tr>
+    <?php
+    }
+    ?>
+</table>
+<div class="ct">
+    <img src="./icon/0411.jpg" onclick="location.href='index.php'">
+    <img src="./icon/0412.jpg" onclick="location.href='?do=checkout'">
+</div>
+
+<?php 
 }else{
     echo "<h2 class='ct'>你的購物車中目前沒有商品，請前往賣場購買</h2>";
 }
